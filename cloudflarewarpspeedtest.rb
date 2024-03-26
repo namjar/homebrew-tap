045af96b1d84e64c9d0041eae48aa26e51b60fd8
namjar <namjar@gmail.com>
2024-03-27 01:21:55 +0800

    Update cloudflarewarpspeedtest.rb

--- cloudflarewarpspeedtest.rb
+++ cloudflarewarpspeedtest.rb
@@ -1,6 +1,6 @@
 class Cloudflarewarpspeedtest < Formula
   desc "Speedtest tool for Cloudflare Warp"
-  homepage "https://github.com/peanut996/CloudflareWarpSpeedTest"
+  homepage "https://github.com/namjar/CloudflareWarpSpeedTest"
   url "https://github.com/peanut996/CloudflareWarpSpeedTest/archive/refs/tags/v1.3.2.tar.gz"
   sha256 "f7a0435c5ad3c801d2fa61aa519ab3117c5ad70ebbd200ac3662086678bee284"
   license "GPL-3.0-only"
@@ -9,7 +9,7 @@ class Cloudflarewarpspeedtest < Formula
 
   def install
     ENV["GOPATH"] = buildpath
-    path = buildpath/"src/github.com/peanut996/CloudflareWarpSpeedTest"
+    path = buildpath/"src/github.com/namjar/CloudflareWarpSpeedTest"
     path.install Dir["*"]
     cd path do
       system "go", "build", "-ldflags", "-X main.Version=v1.3.2", "-o", "#{bin}/CloudflareWarpSpeedTest"
